f8ea819bfbc944670c0b06dc36faae7c50b0768a
Mirza Atif Beg <mirzaatifbeg@Mirzas-MacBook-Air.local>
2025-06-27 15:33:11 +0530

    adding the basic setup Project

---
+++ src/test/java/com/mirza/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirza.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
