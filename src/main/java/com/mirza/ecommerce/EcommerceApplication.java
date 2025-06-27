059d21421c773f2073ee641cffe2ea83e01eb558
Mirza Atif Beg <mirzaatifbeg@Mirzas-MacBook-Air.local>
2025-06-27 15:52:48 +0530

    added basic dot env library setup

--- src/main/java/com/mirza/ecommerce/EcommerceApplication.java
+++ src/main/java/com/mirza/ecommerce/EcommerceApplication.java
@@ -1,12 +1,17 @@
 package com.mirza.ecommerce;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcommerceApplication {
 
+
 	public static void main(String[] args) {
+		Dotenv dotenv=Dotenv.configure().load();
+		dotenv.entries().forEach(entry->System.setProperty(entry.getKey(),entry.getValue()));
+
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
 
