0ab0c6e8f5c8ca29a05a4787584de9649f760ed6
Mirza Atif Beg <mirzaatifbeg@Mirzas-MacBook-Air.local>
2025-07-09 16:19:07 +0530

    adding Interfaces

---
+++ src/main/java/com/mirza/ecommerce/gateway/interfaces/IProductGateway.java
@@ -0,0 +1,4 @@
+package com.mirza.ecommerce.gateway.interfaces;
+
+public class IProductGateway {
+}
---
+++ src/main/java/com/mirza/ecommerce/service/interfaces/IUserService.java
@@ -0,0 +1,4 @@
+package com.mirza.ecommerce.service.interfaces;
+
+public interface IUserService {
+}
